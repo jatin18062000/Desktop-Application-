6671fa6eba211af4cf54a52bf9d521a6a8ad7c1c
Vishwajeet Roundhal <vishwajeetroundhal0@gmail.com>
2025-05-21 17:20:06 +0530

    apijs

---
+++ Backend/api.js
@@ -0,0 +1,8 @@
+
+
+// http://localhost:5000/api/v1/auth/login   need email pass
+
+// http://localhost:5000/api/v1/auth/create-user need name email password with token
+ 
+// http://localhost:5000/api/v1/auth/verify  need email and code 
+
